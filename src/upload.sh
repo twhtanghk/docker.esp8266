40ff25fad2b0f66f88cdbfc8f6836b0274e89c4a
Tommy Tang <twhtanghk@gmail.com>
2017-03-19 23:32:58 +0800

    also compile lua to lc

--- src/upload.sh
+++ src/upload.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
 nodemcu-tool mkfs
-prog=""
-for i in init app wlan req res log router middleware controller str; do
-  prog="${prog} $i.lua"
+prog="init app wlan req res log router middleware controller str"
+moon=""
+lua=""
+lc=""
+for i in ${prog}; do
+  moon="${moon} $i.moon"
+  lua="${lua} $i.lua"
+  lc="${lc} $i.lc"
+  moonc $i.moon
+  luac -o $i.lc $i.lua
 done
-nodemcu-tool upload ${prog}
+nodemcu-tool upload ${lua}
