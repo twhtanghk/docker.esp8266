ea38c370121d6f7c7004feb6207c3241d7e7e631
root <twhtanghk@gmail.com>
2018-02-05 15:37:20 +0000

    add form-generator

---
+++ device/src/sta.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class='panel-body'>
+    <vue-form-generator :schema='schema' :model='model' :options='opts'>
+    </vue-form-generator>
+  </div>
+</template>
+
+<script lang='coffee'>
+module.exports =
+  data: ->
+    model:
+      host: 'host'
+    schema:
+      fields: [
+        { type: 'input', inputType: 'text', label: 'host', model: 'host' }
+      ]
+    opts:
+      validateAfterLoad: true
+      validateAfterChanged: true
+</script>
+
+<style lang='scss'>
+</style>
