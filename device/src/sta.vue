f620d8b271030c31eb4f6470d9cb17d5d618513e
root <twhtanghk@gmail.com>
2018-02-06 15:22:38 +0800

    add restful remote api

--- device/src/sta.vue
+++ device/src/sta.vue
@@ -8,16 +8,38 @@
 <script lang='coffee'>
 module.exports =
   data: ->
+    essidList: []
     model:
-      host: 'host'
+      host: ''
+      essid: ''
+      password: ''
     schema:
       fields: [
         { type: 'input', inputType: 'text', label: 'host', model: 'host' }
+        { type: 'select', label: 'essid', model: 'essid', values: [] }
+        { type: 'input', inputType: 'password', label: 'password', model: 'password' }
       ]
     opts:
       validateAfterLoad: true
       validateAfterChanged: true
+  beforeCreate: ->
+    url =
+      host: 'http://192.168.4.1/wlan/sta'
+      essid: 'http://192.168.4.1/wlan/sta/scan'
+    fetch url.host
+      .then (res) =>
+        @model.host = res.body
+      .catch (err) =>
+        @model.host = err
+    fetch url.essid
+      .then (res) =>
+        @essidList = res.body
+      .catch console.error
 </script>
 
-<style lang='scss'>
+<style lang='scss' scoped>
+  div.panel-body {
+    margin-top: 1em;
+    margin-bottom: 1em;
+  }
 </style>
