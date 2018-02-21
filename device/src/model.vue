c00339a379a9bb0840b4df9dd2d1d28d6f4b6939
root <twhtanghk@gmail.com>
2018-02-21 11:32:04 +0800

    update handler for wlan.ap add http request lib model.vue

---
+++ device/src/model.vue
@@ -0,0 +1,43 @@
+<script lang='coffee'>
+module.exports =
+  param: (data) ->
+    ret = new URLSearchParams()
+    for k, v of data
+      ret.set k, v
+    ret
+
+  opts: (method, data) ->
+    method: method
+    body: module.exports.param data
+    headers:
+      'Content-Type': 'application/x-www-form-urlencoded'
+
+  method: (method, url, data) ->
+    error = (res) ->
+      if res.status != 200
+        throw new Error res.statusText
+      res
+    switch method
+      when 'GET'
+        if data?
+          url = "#{url}?#{module.exports.param data}"
+        fetch url
+          .then error
+      when 'POST', 'PUT', 'DELETE'
+        fetch url, module.exports.opts(method, data)
+          .then error
+      else
+        Promise.reject new Error "unknown http method #{method}"
+    
+  get: (url, data) ->
+    module.exports.method 'GET', url, data
+
+  post: (url, data) ->
+    module.exports.method 'POST', url, data
+
+  put: (url, data) ->
+    module.exports.method 'PUT', url, data
+
+  del: (url, data) ->
+    module.exports.method 'DELETE', url, data
+</script>
