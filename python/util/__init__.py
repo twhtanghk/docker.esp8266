d7547320118d1ae9301b0b301bf22d4ae86093b4
root <twhtanghk@gmail.com>
2018-02-21 11:29:59 +0800

    add error handler

--- python/util/__init__.py
+++ python/util/__init__.py
@@ -1,3 +1,9 @@
+import picoweb
+
 def notFound(req, res):
   yield from picoweb.start_response(res, status='404')
   yield from res.awrite('404\r\n')
+
+def error(req, res, msg='bad request'):
+  yield from picoweb.start_response(res, status='401')
+  yield from res.awrite("{}\r\n".format(msg))
