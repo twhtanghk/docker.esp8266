f476a4ead7c057ce5d210da54a5c2ba31c5f699d
root <twhtanghk@gmail.com>
2018-02-22 17:11:24 +0800

    runtime update tab content

--- device/src/App.vue
+++ device/src/App.vue
@@ -1,32 +1,43 @@
 <template>
-  <b-tabs>
+  <b-tabs @input='select($event)'>
     <b-tab title='AP' active>
-      <ap/>
+      <div id='ap' />
     </b-tab>
     <b-tab title='STA'>
-      <sta/>
+      <div id='sta' />
     </b-tab>
     <b-tab title='PWM'>
-      <pwm/>     
+      <div id='pwm' />
     </b-tab>
   </b-tabs>
 </template>
 
 <script lang='coffee'>
 Vue = require('vue').default
+error = console.error
 console.error = (msg) ->
+  error msg
   Vue.toasted.error msg, duration: 5000
+info = console.info
 console.info = (msg) ->
+  info msg
   Vue.toasted.info msg, duration: 5000
-pwm = require('./pwm').default
-ap = require('./ap').default
-sta = require('./sta').default
+AP = Vue.extend require('./ap').default
+STA = Vue.extend require('./sta').default
+PWM = Vue.extend require('./pwm').default
 
 module.exports =
-  components:
-    ap: ap
-    sta: sta
-    pwm: pwm
+  methods:
+    select: (tabIndex) ->
+      component = @$children[0].$children[tabIndex]
+      id = component.$el.id
+      switch tabIndex
+        when 0
+          new AP el: "#ap"
+        when 1
+          new STA el: "#sta"
+        when 2
+          new PWM el: "#pwm"
 </script>
 
 <style lang='scss'>
