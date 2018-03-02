0fcb8a8690a779d8c0f10de619a2efc88541c374
root <twhtanghk@gmail.com>
2018-03-02 18:01:09 +0800

    clear and upload files in flash memory

---
+++ flash.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+rshell -a -b 115200 --buffer-size=100 -p /dev/ttyUSB0 <<EOF
+rm -rf /pyboard/*
+mkdir /pyboard/static
+rsync -v $HOME/docker.esp8266/device/dist /pyboard/static
+rsync -v $HOME/docker.esp8266/python /pyboard
+rsync -v $HOME/.micropython /pyboard
+EOF
