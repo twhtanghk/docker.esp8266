9fefae97d0269de897ed15e8622f491179ec5e3a
root <twhtanghk@gmail>
2018-06-16 11:46:06 +0800

    enable webrepl console

--- python/boot.py
+++ python/boot.py
@@ -1,8 +1,8 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
 #import esp
 #esp.osdebug(None)
-#import webrepl
-#webrepl.start()
+import webrepl
+webrepl.start()
 from config import model as cfg
 cfg.boot()
 pkg = ['ap', 'gpio', 'ddns']
