9a5d61d35f9c81af9f145eee07b9f97b8118b106
root <twhtanghk@gmail.com>
2018-12-23 15:17:50 +0000

    search for static files from default directory ../statics

--- python/util/__init__.py
+++ python/util/__init__.py
@@ -39,7 +39,7 @@ def handler(f):
   return ret
 
 def static(req, res):
-  file = '..' + req.url_match.group(1)
+  file = '../statics' + req.url_match.group(1)
   mime = picoweb.get_mime_type(file)
   app = picoweb.WebApp(__name__)
   if b'gzip' in req.headers[b'Accept-Encoding']:
