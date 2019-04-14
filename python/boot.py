c20a1e76e4fba84e0fe973807061b276e92ed2f4
root <twhtanghk@gmail.com>
2019-04-14 18:48:20 +0000

    disable osdebug and call module.boot instead of setup

--- python/boot.py
+++ python/boot.py
@@ -1,11 +1,12 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
-#import esp
-#esp.osdebug(None)
+import esp
+esp.osdebug(None)
 import webrepl
 webrepl.start()
 import pkg
 for i in pkg.list:
   lib = __import__(i)
   lib.model.boot()
+  lib.model.setup()
 import gc
 gc.collect()
