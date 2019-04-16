59370d9514440014ac3f0a78fcb14a77794f5227
root <twhtanghk@gmail.com>
2019-04-16 10:54:48 +0000

    move util.exists to config move rest api factory, reset, read to system.py

--- python/config.py
+++ python/config.py
@@ -1,16 +1,18 @@
 import ujson
-import picoweb
-from util import handler, ok
+
+def exists(filename):
+  try:
+    import os
+    os.stat(filename)
+    return True
+  except OSError:
+    return False
 
 class Config:
   def __init__(self, filename):
     self.filename = filename
 
-  def setup(self):
-    return
-
   def boot(self):
-    from util import exists
     if not exists(self.filename):
       self.factory()
 
@@ -25,29 +27,3 @@ class Config:
     ujson.dump(data, f)
     f.close()
     return self
-
-class Controller:
-  def __init__(self):
-    return
-
-  def factory(self, req, res):
-    import pkg
-    for i in pkg.list:
-      lib = __import__(i)
-      lib.model.factory()
-    yield from ok(res)
-
-  def reset(self, req, res):
-    yield from ok(res)
-    import machine
-    machine.reset()
-
-  def read(self, req, res):
-    data = {}
-    import pkg
-    for i in pkg.list:
-      lib = __import__(i)
-      data[i] = lib.model.load()
-    yield from ok(res, data)
-
-ctl = Controller()
