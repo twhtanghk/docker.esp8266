99e4e60b8e06b43f18792420e3924597204de357
root <twhtanghk@gmail.com>
2019-04-16 12:29:22 +0000

    mv gpio rest api to model update rest api handler

--- python/gpio.py
+++ python/gpio.py
@@ -19,60 +19,47 @@ class Model(Config):
     })
     return self
 
-  def setup(self):
+  def boot(self):
     self.cfg = self.load()
     for name in self.cfg:
       self.pins[name] = Pin(self.cfg[name]['id'], self.cfg[name]['mode'], self.cfg[name]['pull'])
       self.pins[name].value(self.cfg[name]['value'])
 
-  def set(self, name, value):
+  def _set(self, name, value):
     self.pins[name].value(value)
-    return self.get(name)
+    return self._get(name)
 
-  def on(self, name):
+  def _on(self, name):
     return self.set(name, 1)
 
-  def off(self, name):
+  def _off(self, name):
     return self.set(name, 0)
  
-  def get(self, name):
+  def _get(self, name):
     return {
       'name': name,
       'pin': self.cfg[name]['id'],
+      'mode': self.cfg[name]['mode'],
+      'pull': self.cfg[name]['pull'],
       'value': self.pins[name].value()
     }
 
-  def list(self):
+  def _list(self):
     ret = []
     for name in self.cfg:
-      ret.append(self.get(name))
+      ret.append(self._get(name))
     return ret
 
-class Controller:
-  def __init__(self, model):
-    self.model = model
-
   def list(self, req, res):
-    yield from ok(res, self.model.list())
+    yield from ok(res, self._list())
 
   def read(self, req, res):
-    yield from ok(res, self.model.get(req.params['name']))
+    yield from ok(res, self._get(req.params['name']))
 
   def update(self, req, res):
     yield from req.read_form_data()
-    name = req.params['name']
+    name = req.url_match.group(1)
     value = ujson.loads(req.form['value'][0])
-    yield from ok(res, self.model.set(name, value))
-
-  def crud(self, req, res):
-    req.params = {
-      'name': req.url_match.group(1)
-    }
-    ret = {
-      'GET': self.read,
-      'PUT': self.update
-    }
-    yield from ret[req.method](req, res)
+    yield from ok(res, self._set(name, value))
 
 model = Model()
-ctl = Controller(model)
--- python/main.py
+++ python/main.py
@@ -1,6 +1,6 @@
 import ure as re
 import picoweb
-import config
+import system
 import ap
 import sta
 import log
@@ -12,18 +12,18 @@ import util
 util.inetd()
 
 routes = [
-  ('/cfg', util.handler(config.ctl.read)),
-  ('/cfg/reset', util.handler(config.ctl.reset)),
-  ('/cfg/factory', util.handler(config.ctl.factory)),
+  ('/cfg', util.handler2({'GET': system.model.read})),
+  ('/cfg/reset', util.handler2({'GET': system.model.reset})),
+  ('/cfg/factory', util.handler2({'GET': system.model.factory})),
   ('/sta', util.handler2({'GET': sta.model.status, 'PUT': sta.model.set})),
   ('/sta/scan', util.handler(sta.model.scan)),
-  ('/ap', util.handler(ap.ctl.crud)),
+  ('/ap', util.handler2({'GET': ap.model.get, 'PUT': ap.model.set})),
   ('/log', util.handler2({'GET': log.model.status, 'PUT': log.model.set})),
   ('/pwm', util.handler(pwm.ctl.list)),
   (re.compile('^/pwm/(\w+)$'), util.handler(pwm.ctl.crud)),
   (re.compile('^/pwm/(\w+)/duty$'), util.handler(pwm.ctl.duty)),
-  ('/gpio', util.handler(gpio.ctl.list)),
-  (re.compile('^/gpio/(\w+)$'), util.handler(gpio.ctl.crud)),
+  ('/gpio', util.handler2({'GET': gpio.model.list})),
+  (re.compile('^/gpio/(\w+)$'), util.handler2({'GET': gpio.model.read, 'PUT': gpio.model.update})),
   (re.compile('^/ddns$'), util.handler(ddns.ctl.crud)),
   (re.compile('^(.*)$'), util.handler(util.static))
 ]
