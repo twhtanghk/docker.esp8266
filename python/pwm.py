da07edccefb7aa60eabf80f97f7ec9e0187a4a8f
root <twhtanghk@gmail.com>
2019-04-20 16:27:33 +0000

    combine pwm controller methods into model

--- python/pwm.py
+++ python/pwm.py
@@ -1,52 +1,12 @@
 import ujson
 from config import Config
-from util import handler, ok
-import ure as re
+from util import ok
 import log as logging
 logger = logging.getLogger(__name__)
 
-class Controller:
-  def __init__(self, model):
-    self.model = model
-
-  def list(self, req, res):
-    yield from ok(res, model.list())
-
-  def read(self, req, res):
-    yield from ok(res, model.read(req.params))
-    
-  def update(self, req, res):
-    yield from req.read_form_data()
-    req.params = {
-      'device': req.url_match.group(1),
-      'pin': int(req.form['pin']),
-      'default': int(req.form['default'])
-    }
-    model.update(req.params)
-    yield from ok(res)
-
-  def duty(self, req, res):
-    req.params = {
-      'device': req.url_match.group(1)
-    }
-    yield from req.read_form_data()
-    req.params['value'] = int(req.form['value'])
-    self.model.duty(req.params)
-    yield from ok(res)
-  
-  def crud(self, req, res):
-    req.params = {
-      'device': req.url_match.group(1)
-    }
-    ret = {
-      'GET': self.read,
-      'PUT': self.update
-    }
-    yield from ret[req.method](req, res)
-
 class Model(Config):
   def __init__(self):
-    Config.__init__(self, 'pwm.json')
+    Config.__init__(self, '/pwm.json')
 
   def factory(self):
     self.save({
@@ -71,7 +31,7 @@ class Model(Config):
       dev.duty(default)
     logger.info(ujson.dumps(cfg))
 
-  def list(self):
+  def _list(self):
     cfg = self.load()
     for name in cfg:
       pin = cfg[name]['pin']
@@ -82,14 +42,14 @@ class Model(Config):
       }
     return cfg
     
-  def read(self, opts):
+  def _read(self, opts):
     cfg = self.load()
     name = opts['device']
     pin = cfg[name]['pin']
     cfg[name]['value'] = self.device(pin).duty()
     return cfg[name]
 
-  def update(self, opts):
+  def _update(self, opts):
     cfg = self.load()
     cfg[opts['device']] = {
       'pin': opts['pin'],
@@ -97,9 +57,33 @@ class Model(Config):
     }
     self.save(cfg)
   
-  def duty(self, opts):
+  def _duty(self, opts):
     cfg = self.load()
     self.device(cfg[opts['device']]['pin']).duty(opts['value'])
 
+  def list(self, req, res):
+    yield from ok(res, self._list())
+
+  def read(self, req, res):
+    yield from ok(res, self._read(req.params))
+    
+  def update(self, req, res):
+    yield from req.read_form_data()
+    req.params = {
+      'device': req.url_match.group(1),
+      'pin': int(req.form['pin']),
+      'default': int(req.form['default'])
+    }
+    self._update(req.params)
+    yield from ok(res)
+
+  def duty(self, req, res):
+    req.params = {
+      'device': req.url_match.group(1)
+    }
+    yield from req.read_form_data()
+    req.params['value'] = int(req.form['value'])
+    self._duty(req.params)
+    yield from ok(res)
+
 model = Model()
-ctl = Controller(model)
