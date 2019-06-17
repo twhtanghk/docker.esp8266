580f5abe5e0aad7cd14e5d03604b2a4911db6be7
root <twhtanghk@gmail.com>
2019-06-17 09:46:04 +0000

    add class Req and Res

--- esp8266/http.py
+++ esp8266/http.py
@@ -1,46 +1,87 @@
 import ujson
 
-def ok(socket, data):
-  data = ujson.dumps(data)
-  socket.write("HTTP/2 200\r\n")
-  socket.write("content-type: application/json\r\n")
-  socket.write("content-length: %s\r\n\r\n" % len(data))
-  socket.write(data)
+class Req:
+  def __init__(self, socket):
+    self.socket = socket
+    (method, url, version) = socket.readline().decode('utf-8').split(" ")
+    self.action = "%s %s" % (method, url)
+    self.header = self._header()
+    self.body = {}
+    if b'Content-Length' in self.header:
+      self.body = self._body()
 
-def err(socket, code, msg):
-  socket.write("HTTP/1.1 %s %s\r\n\r\n" % (code, msg))
+  def _header(self):
+    ret = {}
+    while True:
+      l = self.socket.readline()
+      if l == b"\r\n":
+        break
+      k, v = l.split(b":", 1)
+      ret[k] = v.strip()
+    return ret
 
-def header(socket):
-  ret = {}
-  while True:
-    l = socket.readline()
-    if l == b"\r\n":
-      break
-    k, v = l.split(b":", 1)
-    ret[k] = v.strip()
-  return ret
+  def _body(self):
+    len = int(self.header[b'Content-Length'])
+    return ujson.loads(self.socket.recv(len))
 
-def body(socket, len):
-  return ujson.loads(socket.recv(len))
+class Res:
+  def __init__(self, socket):
+    self.socket = socket
 
-def handle(socket):
-  (method, url, version) = socket.readline().split(b" ")
-  action = "%s %s" % (method, url)
-  h = header(socket)
-  b = ''
-  if b'Content-Length' in h:
-    b = body(socket, int(h[b'Content-Length']))
-  ok(socket, {'url': url, 'header': h, 'body': b})
-  socket.close()
+  def ok(self, data):
+    data = ujson.dumps(data)
+    self.socket.write("HTTP/2 200\r\n")
+    self.socket.write("content-type: application/json\r\n")
+    self.socket.write("content-length: %s\r\n\r\n" % len(data))
+    self.socket.write(data)
 
-import usocket
+  def err(self, code, msg):
+    self.socket.write("HTTP/2 %s %s\r\n\r\n" % (code, msg))
 
-server = usocket.socket()
-server.bind(('0.0.0.0', 80))
-server.listen(1)
-while True:
-  try:
-    (socket, sockaddr) = server.accept()
-    handle(socket)
-  except:
-    err(socket, 500, 'Internal Server Error')
+import ure as re
+
+class App:
+  def __init__(self):
+    self.routes = []
+
+  def method(self, method, url, mw):
+    self.routes.append({
+      'action': re.compile('%s %s' % (method, url)),
+      'mw': mw
+    })
+
+  def get(self, url, mw):
+    self.method('GET', url, mw)
+
+  def post(self, url, mw):
+    self.method('POST', url, mw)
+
+  def put(self, url, mw):
+    self.method('PUT', url, mw)
+
+  def delete(self, url, mw):
+    self.method('DELETE', url, mw)
+
+  def handle(self, socket):
+    try:
+      res = Res(socket)
+      req = Req(socket)
+      for route in self.routes:
+        if route['action'].match(req.action):
+          route['mw'](req, res)
+          return
+      res.err(404, 'Not Found')
+    except:
+      res.err(500, 'Internal Server Error')
+    finally:
+      print('socket closed')
+      socket.close()
+
+  def run(self, host='0.0.0.0', port=80):
+    import usocket
+    server = usocket.socket()
+    server.bind((host, port))
+    server.listen(1)
+    while True:
+      (socket, sockaddr) = server.accept()
+      self.handle(socket)
--- esp8266/main.py
+++ esp8266/main.py
@@ -1,2 +1,9 @@
 import net
 import http
+
+def echo(req, res):
+  res.ok(req.__dict__)
+ 
+app = http.App()
+app.get('/.*', echo)
+app.run()
