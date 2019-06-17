c697e88d23ceea40b6db15203d43ae4b240a125d
root <twhtanghk@gmail.com>
2019-06-17 16:52:29 +0000

    fix fomula for current measurment

--- esp8266/multimeter.py
+++ esp8266/multimeter.py
@@ -1,8 +1,7 @@
 import machine
 
 def _voltage(r1=680, r2=220):
-  vin = machine.ADC(0).read()
-  return vin * 3.3 / 1024 * (1 + r1 / r2)
+  return machine.ADC(0).read() * 3.3 / 1024 * (1 + r1 / r2)
 
 def _current(acsRange=20):
   sensitivity = {
@@ -10,7 +9,7 @@ def _current(acsRange=20):
     20: 100,
     30: 66
   }
-  return (_voltage() - acsRange / 2) / sensitivity[acsRange] / 1000
+  return (_voltage() - 2.5) * 1000 / sensitivity[acsRange]
   
 def voltage(req, res):
   res.ok({
