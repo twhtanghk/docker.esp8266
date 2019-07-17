0944a06338c2d096fd37010c501f7401b2c7b08e
root <twhtanghk@gmail.com>
2019-07-17 15:41:11 +0000

    ignore mqtt connection error

--- esp8266/mqtt.py
+++ esp8266/mqtt.py
@@ -2,4 +2,7 @@ import net
 from umqtt.robust import MQTTClient
 
 client = MQTTClient(net.ap['essid'], 'mqtt.flespi.io', port=1883, user='', password='')
-client.connect()
+try:
+  client.connect()
+except OSError as e:
+  pass
