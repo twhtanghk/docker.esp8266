604d5a347b65cf7498bb129c78caf0bd51b4c271
root <twhtanghk@gmail.com>
2019-09-04 14:59:50 +0000

    remove unused modules and keep pwm for fan control only

--- esp8266/main.py
+++ esp8266/main.py
@@ -1,38 +1,21 @@
 import http
 import system
-import mqtt
-from multimeter import voltage, current
-import humidity
-import thermistor
-import gpio
 import pwm
-import liquid
 
 app = http.App()
 app.options('.*', http.preflight)
 app.get('/factory', system.factory)
 app.get('/reboot', system.reboot)
-app.post('/mqtt', mqtt.reset)
 app.get('/ap', system.getAP)
 app.put('/ap', system.configAP)
 app.get('/sta', system.getSTA)
 app.get('/sta/scan', system.hotspot)
 app.put('/sta', system.configSTA)
-app.get('/voltage', voltage)
-app.get('/current', current)
-app.get('/dht', humidity.sensor.json)
-app.get('/thermistor', thermistor.sensor.json)
-app.get('/gpio/(\d+)', gpio.get)
-app.put('/gpio/(\d+)/mode', gpio.mode) # set id: 5, mode: 'in'
-app.put('/gpio/(\d+)/value', gpio.set) # set id: 5, value: 1
 app.get('/pwm/(\d+)', pwm.get)
 app.put('/pwm/(\d+)', pwm.duty) # set id: 5, duty: 600
-app.get('/liquid/(\d+)', liquid.get)
 app.get('(.*)', http.static)
 
 import uasyncio as asyncio
 loop = asyncio.get_event_loop()
 loop.create_task(asyncio.start_server(app.handle, '0.0.0.0', 80))
-#loop.create_task(humidity.publish())
-#loop.create_task(thermistor.publish())
 loop.run_forever()
--- esp8266/stepper.py
+++
@@ -1,3 +0,0 @@
-from uln2003 import Stepper, HALF_STEP
-
-device = Stepper(HALF_STEP, 14, 12, 13, 15)
