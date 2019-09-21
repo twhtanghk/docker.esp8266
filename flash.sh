5fb8d4b860303dfee5c875770abb047027f422d0
root <twhtanghk@gmail.com>
2019-09-21 09:49:29 +0000

    update flash to rsync src under current directory

--- flash.sh
+++ flash.sh
@@ -3,7 +3,6 @@
 rshell -a -b 115200 --buffer-size=100 -p /dev/ttyUSB0 <<EOF
 rm -rf /pyboard/*
 mkdir /pyboard/static
-rsync -v $HOME/docker.esp8266/frontend/dist /pyboard/static
-rsync -v $HOME/docker.esp8266/python /pyboard
-rsync -v $HOME/.micropython /pyboard
+rsync -v frontend/dist /pyboard/static
+rsync -v esp8266 /pyboard
 EOF
