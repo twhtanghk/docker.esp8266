7f17855a8de67ea134c979d33c5745ae12983d3f
root <twhtanghk@gmail.com>
2020-03-01 08:33:26 +0000

    add background task to adjust antenna heading

--- esp8266/main.py
+++ esp8266/main.py
@@ -1,5 +1,6 @@
 import http
 import system
+import heading
 import antenna
 import stepper
 import opto
@@ -13,6 +14,8 @@ app.put('/ap', system.configAP)
 app.get('/sta', system.getSTA)
 app.get('/sta/scan', system.hotspot)
 app.put('/sta', system.configSTA)
+app.put('/heading/boat/(-?\d*[.]?\d*)$', heading.boat)
+app.put('/heading/antenna/(-?\d*[.]?\d*)$', heading.antenna)
 app.put('/antenna/(-?\d*[.]?\d*)$', antenna.angle)
 app.put('/stepper/(-?\d*[.]?\d*)$', stepper.angle)
 app.get('/opto', opto.get)
@@ -20,5 +23,6 @@ app.get('(.*)', http.static)
 
 import uasyncio as asyncio
 loop = asyncio.get_event_loop()
+loop.create_task(heading.adjust())
 loop.create_task(asyncio.start_server(app.handle, '0.0.0.0', 80))
 loop.run_forever()
