b29daf7149095b2ae8baaba6fbbc944ec18fb79a
root <twhtanghk@gmail.com>
2022-11-08 13:59:37 +0000

    reshuffle the function order

--- python/config.py
+++ python/config.py
@@ -4,22 +4,6 @@ import ujson as json
 app = Microdot()
 filename = '/config.json'
 
-@app.get('/reset')
-def reset(req):
-  import machine
-  machine.reset()
-  return ''
-
-@app.get('/factory')
-def factory(req):
-  from project import pkg
-  ret = {}
-  for i in pkg:
-    if i != 'config':
-      lib = __import__(i)
-      ret[i] = lib.factory()
-  return ''
-
 def read():
   try:
     f = open(filename)
@@ -36,6 +20,23 @@ def write(data):
   f.write(json.dumps(data))
   f.close()
 
+@app.get('/reset')
+def reset(req):
+  import machine
+  machine.reset()
+  return ''
+
+@app.get('/factory')
+def factory(req):
+  from project import pkg
+  ret = {}
+  for i in pkg:
+    if i != 'config':
+      lib = __import__(i)
+      ret[i] = lib.factory()
+  write(ret)
+  return ''
+
 @app.get('/')
 def load(req):
   ret = read()
@@ -50,5 +51,5 @@ def load(req):
 def save(req):
   # validate json data before write back
   data = req.json
-  write()
+  write(data)
   return ''
