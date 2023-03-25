bda5a200287c02438c59729dbb01ec0222903a08
root <twhtanghk@gmail.com>
2023-03-25 14:32:56 +0000

    show pin state on title bar

--- frontend/src/gpio.vue
+++ frontend/src/gpio.vue
@@ -1,7 +1,7 @@
 <template>
   <v-row>
     <v-col>
-      <card :header="id">
+      <card :header="id + ': '  + (state == '1' ? 'high (off)' : 'low (on)')">
         <v-btn color='primary' variant='outlined' @mousedown='off' @touchstart='off' @mouseup='on' @touchend='on'>
           {{id}}
         </v-btn>
@@ -18,15 +18,15 @@ export default
   components: {card}
   data: ->
     id: 'switch'
+    state: null
   methods:
     on: ->
-      await gpio.put url: "/gpio/#{@id}/on"
+      {@state} = await gpio.put url: "/gpio/#{@id}/on"
+      @state = state
     off: ->
-      await gpio.put url: "/gpio/#{@id}/off"
-    get: ->
-      val = await gpio.read data: {@id}
+      {@state} = await gpio.put url: "/gpio/#{@id}/off"
   mounted: ->
-    await @get()
+    {@state} = await gpio.read data: {@id}
 </script>
 
 <style lang='scss'>
